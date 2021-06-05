eb638e0f09818c52f4bc515d42de9d47a4049468
CuzImSyntax <62243375+CuzImSyntax@users.noreply.github.com>
2021-06-05 22:26:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
 import setuptools
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="TrivIO",
     version="0.1",
     license="MIT",
     author="CuzImSyntax",
     description="An async opentdb API wrapper written in python.",
-    long_description="Coming Soon",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/CuzImSyntax",
+    url="https://github.com/CuzImSyntax/TrivIO",
     classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
     python_requires='>=3.7.0',
-    packages=setuptools.find_packages(where="trivio"),
+    packages=requirements,
 
 )
