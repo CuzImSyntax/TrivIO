776601fcf1569f847d3b9104b7ae8600f306b8a1
CuzImSyntax <62243375+CuzImSyntax@users.noreply.github.com>
2021-06-05 20:11:13 +0200

    Update decoder

--- trivio/utils.py
+++ trivio/utils.py
@@ -5,17 +5,22 @@ import binascii
 class Utils:
     """"Class with helping functions"""
 
-    def decode(self, item):
+    def build_dict(self, item):
         """"Decodes a given query"""
+        result = {"response_code": item["response_code"], "results": []}
+        for question in item["results"]:
+            result["results"].append({key: self.decode(value) for key, value in question.items()})
+        return result
 
+    def decode(self, item):
         if isinstance(item, int):
             return item
         elif isinstance(item, str):
             return self._decode(item)
         elif isinstance(item, list):
-            return [self.decode(i) for i in item]
+            return [self.decode(x) for x in item]
         elif isinstance(item, dict):
-            return {key: self.decode(value) for key, value in item.values()}
+            return {key: self.decode(value) for key, value in item.items()}
 
     def _decode(self, item):
         """"Decodes a base64 string to an utf8 string"""
