a92c185de0a92650707906c13b1a34dd312de074
CuzImSyntax <62243375+CuzImSyntax@users.noreply.github.com>
2021-06-05 22:26:51 +0200

    testing functions

--- tests/testing.py
+++ tests/testing.py
@@ -4,9 +4,8 @@ import asyncio
 
 async def main():
     client = trivio.Client(True)
-    result = await client.request(trivio.Type.TRUE_FALSE, 1, trivio.Category.ART, trivio.Difficulty.EASY)
+    questions = await client.request(trivio.Type.MULTIPLE_CHOICE, 1, trivio.Category.ALL, trivio.Difficulty.EASY)
     await client.close()
-    print(result)
-
+    print(questions)
 
 asyncio.run(main())
