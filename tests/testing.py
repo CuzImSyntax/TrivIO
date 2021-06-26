5155ea242b174ba9bedfe3f72b69beb0a1533459
CuzImSyntax <62243375+CuzImSyntax@users.noreply.github.com>
2021-06-26 14:46:11 +0200

    Testing

--- tests/testing.py
+++ tests/testing.py
@@ -4,7 +4,7 @@ import asyncio
 
 async def main():
     client = trivio.Client(use_token=True)
-    questions = await client.request(trivio.Type.MULTIPLE_CHOICE, 1, trivio.Category.ALL, trivio.Difficulty.EASY)
+    questions = await client.request(trivio.Type.TRUE_FALSE, 10, trivio.Category.ART, trivio.Difficulty.HARD)
     await client.close()
     print(questions)
 
